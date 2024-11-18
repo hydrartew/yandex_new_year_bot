ac3410c32c5119c8c6192cd7622257d3435da9cf
hydrartew <hydrartew@mail.ru>
2024-11-18 16:59:18 +0300

    добавлена ф-ция get_snow_stats

--- db/db_redis/snow.py
+++ db/db_redis/snow.py
@@ -12,12 +12,16 @@ logging.getLogger('db.redis').propagate = False
 logger = logging.getLogger('db.redis')
 
 
-@retry(
-    stop=stop_after_attempt(5),  # Максимальное количество попыток
-    wait=wait_exponential(min=1, max=4),  # Экспоненциальная задержка между попытками
-    before_sleep=before_sleep_log(logger, logging.INFO)  # Логирование перед каждой попыткой
-)
-def snow_plus_one(tg_user_id: int, prefix: str = 'tg_user_id'):
+def redis_retry():
+    return retry(
+        stop=stop_after_attempt(5),
+        wait=wait_exponential(min=1, max=4),
+        before_sleep=before_sleep_log(logger, logging.INFO)
+    )
+
+
+@redis_retry()
+def snow_plus_one(tg_user_id: int, prefix: str = 'tg_user_id:snow') -> None:
     key = '{}:{}'.format(prefix, tg_user_id)
 
     logger.info(f'/snow {key}')
@@ -34,4 +38,27 @@ def snow_plus_one(tg_user_id: int, prefix: str = 'tg_user_id'):
         raise
 
 
+@redis_retry()
+def get_snow_stats(tg_user_id: int, prefix: str = 'tg_user_id:snow') -> int | None:
+    key = '{}:{}'.format(prefix, tg_user_id)
+
+    logger.info(f'/snow stats {key}')
+    try:
+        value = r.get(key)
+    except redis.ConnectionError as e:
+        logger.error(f'Error connecting to Redis: {e}')
+        raise
+    except redis.TimeoutError as e:
+        logger.error(f'Timeout when trying to connect to Redis: {e}')
+        raise
+    except Exception as e:
+        logger.critical(f"An unexpected error: {e}")
+        raise
+
+    if value is None:
+        logger.warning(f'/snow stats value is None for {key}')
+    return value
+
+
 snow_plus_one(123)
+print(get_snow_stats(1234))
