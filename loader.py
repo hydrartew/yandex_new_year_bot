a92d3e55e33ff521592d2447715aca62037e2a45
hydrartew <hydrartew@mail.ru>
2024-11-19 15:38:21 +0300

    добавлено redis storage хранилище

--- loader.py
+++ loader.py
@@ -1,6 +1,18 @@
 from aiogram import Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
+import redis.asyncio as aioredis
 
-storage = MemoryStorage()
+from config_data import settings
+
+storage = RedisStorage(
+    redis=aioredis.StrictRedis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        password=settings.REDIS_PASSWORD.get_secret_value(),
+        ssl=True,
+        ssl_ca_certs=settings.path_ssl_ca_certs,
+        decode_responses=True
+    )
+)
 
 dp = Dispatcher(storage=storage)
