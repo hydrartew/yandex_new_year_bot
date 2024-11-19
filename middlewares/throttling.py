99f63342834d294736a89f984ef6eb0868e68122
hydrartew <hydrartew@mail.ru>
2024-11-19 13:34:42 +0300

    изменен middleware

--- middlewares/throttling.py
+++ middlewares/throttling.py
@@ -1,25 +1,27 @@
 import logging
-from typing import Any, Awaitable, Callable, Dict
+from typing import Any, Awaitable, Callable
 
 from aiogram import BaseMiddleware
 from aiogram.types import Message
 from cachetools import TTLCache
+from icecream import ic
 
 logger = logging.getLogger('middleware')
 
 
 class ThrottlingMiddleware(BaseMiddleware):
-    def __init__(self, time_limit: int = 5):
+    def __init__(self, time_limit: int = 2):
         self.limit = TTLCache(maxsize=10_000, ttl=time_limit)
 
     async def __call__(
-            self,
-            handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-            event: Message,
-            data: Dict[str, Any]
+        self,
+        handler: Callable[[Message, dict[str, Any]], Awaitable[Any]],
+        event: Message,
+        data: dict[str, Any]
     ) -> Any:
-        if event.chat.id in self.limit:
-            logger.info(f'{event.chat} blocked {self.limit} seconds')
+        # ic(self.limit)
+        if event.from_user.id in self.limit:
+            logger.info(f'tg_user_id:{event.from_user.id} blocked {self.limit.ttl} seconds')
             return
-        self.limit[event.chat.id] = None
+        self.limit[event.from_user.id] = None
         return await handler(event, data)
