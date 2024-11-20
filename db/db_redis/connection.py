23e09123b45a9c120da6d67238af36644fd8d4b0
hydrartew <hydrartew@mail.ru>
2024-11-20 17:48:21 +0300

    redis_retry вынесен в отдельный файл

--- db/db_redis/connection.py
+++ db/db_redis/connection.py
@@ -1,6 +1,13 @@
+import logging
+
+from tenacity import retry, stop_after_attempt, wait_exponential, before_sleep_log
+
 from config_data import settings
 
 import redis.asyncio as aioredis
+
+
+logger = logging.getLogger('db.redis')
 
 
 async def create_redis_client() -> aioredis.StrictRedis:
@@ -11,4 +18,12 @@ async def create_redis_client() -> aioredis.StrictRedis:
         ssl=True,
         ssl_ca_certs=settings.path_ssl_ca_certs,
         decode_responses=True
+    )
+
+
+def redis_retry():
+    return retry(
+        stop=stop_after_attempt(5),
+        wait=wait_exponential(min=1, max=4),
+        before_sleep=before_sleep_log(logger, logging.INFO)
     )
