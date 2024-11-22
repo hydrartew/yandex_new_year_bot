732a9b3fb383b766dd16948a41e5c3dfca52e5af
hydrartew <yambarshevartem@yandex.ru>
2024-11-22 17:38:49 +0300

    добавлены схемы для игры snow_duel

--- schemas.py
+++ schemas.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from enum import StrEnum, Enum
+from typing import Literal
 
 from pydantic import BaseModel
 
@@ -24,3 +26,32 @@ class SnowmanRedisData(BaseModel):
     all_attempts: int = 0
     current: int = 0
     maximum: int = 0
+
+
+class SnowDuelUser(BaseModel):
+    tg_user_id: int
+    points: int = 0
+    moves: int = 0
+    dttm_last_move: datetime | None = None
+
+
+class WhoMoves(Enum):
+    owner = 'owner'
+    opponent = 'opponent'
+
+
+class SnowDuelRoom(BaseModel):
+    game_status: Literal['created', 'in_progress', 'finished', 'cancelled']
+    owner: SnowDuelUser
+    opponent: SnowDuelUser | None = None
+    who_moves: WhoMoves | None = None
+    current_round: int = 0
+    distance: int
+    dttm_created: datetime
+
+
+class MakeMove(BaseModel):
+    user_in_room: bool = True
+    is_current_user_move: bool = True
+    room_exists: bool = True
+    snow_duel_data: SnowDuelRoom | None = None
