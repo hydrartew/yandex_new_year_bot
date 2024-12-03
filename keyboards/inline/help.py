09ade51341b1d49a7991a6be78b5d4b96139a8de
hydrartew <yambarshevartem@yandex.ru>
2024-12-04 01:08:28 +0300

    заменен текст для /help и расположение кнопок

--- keyboards/inline/help.py
+++ keyboards/inline/help.py
@@ -4,12 +4,12 @@ ikb_welcome_private_chat = InlineKeyboardMarkup(
         inline_keyboard=[
             [
                 InlineKeyboardButton(
-                    text=f'Форма',
-                    url='https://forms.yandex-team.ru/ext/surveys/13711111/'
-                ),
-                InlineKeyboardButton(
                     text=f'YNYB News',
                     url='https://nda.ya.ru/t/8Ve9IRKc79adW7'
+                ),
+                InlineKeyboardButton(
+                    text=f'Форма',
+                    url='https://forms.yandex-team.ru/ext/surveys/13711111/'
                 )
             ],
             [
@@ -26,12 +26,12 @@ ikb_welcome_group_chat = InlineKeyboardMarkup(
         inline_keyboard=[
             [
                 InlineKeyboardButton(
-                    text=f'Форма',
-                    url='https://forms.yandex-team.ru/ext/surveys/13711111/'
-                ),
-                InlineKeyboardButton(
                     text=f'YNYB News',
                     url='https://nda.ya.ru/t/8Ve9IRKc79adW7'
+                ),
+                InlineKeyboardButton(
+                    text=f'Форма',
+                    url='https://forms.yandex-team.ru/ext/surveys/13711111/'
                 )
             ]
         ]
