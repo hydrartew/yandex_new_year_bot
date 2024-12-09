cd3f7a488787bb9b5637f4528535d09fda79964f
hydrartew <yambarshevartem@yandex.ru>
2024-12-09 22:43:26 +0300

    добавлена ru локализация для /quiz

--- handlers/games/quiz.py
+++ handlers/games/quiz.py
@@ -1,15 +1,16 @@
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram_i18n import I18nContext
 
 from configs import settings
 from filters import GroupChat, IsSubscribed
 from loader import dp
+from localization import Localization
 
 
 @dp.message(Command('quiz'), GroupChat(), IsSubscribed(), flags={"throttling_key": "quiz"})
-async def game_quiz(message: Message) -> None:
-    await message.reply(
-        'Игра пока недоступна, следи за обновлениями в телеграм канале <a href="{}">{}</a>'.format(
-            settings.TELEGRAM_CHANNEL_BOT_NEWS_INVITE_LINK, settings.TELEGRAM_CHANNEL_BOT_NEWS_NAME
+async def game_quiz(message: Message, i18n: I18nContext) -> None:
+    await message.reply(text=Localization(message, i18n).get(
+            'quiz', TELEGRAM_CHANNEL_BOT_NEWS_INVITE_HYPERLINK=settings.TELEGRAM_CHANNEL_BOT_NEWS_INVITE_HYPERLINK
         )
     )
