e15456b8ef9f200b4cdfecd80b7ba87c5b51e398
hydrartew <yambarshevartem@yandex.ru>
2024-12-10 13:15:16 +0300

    добавлена ru локализация для /stats

--- handlers/users/stats.py
+++ handlers/users/stats.py
@@ -1,32 +1,36 @@
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram_i18n import I18nContext
 
 from configs import settings
 from db import db_redis, db_ydb
 from filters import IsSubscribed
 
 from loader import dp
+from localization import Localization
 
 
 @dp.message(Command('stats', 'profile'), IsSubscribed(), flags={"throttling_key": "stats"})
-async def my_stats(message: Message) -> None:
+async def my_stats(message: Message, i18n: I18nContext) -> None:
+    localization = Localization(message, i18n)
 
     game_stats = await db_redis.get_stats(message.from_user.id)
     prediction_stats = await db_ydb.get_prediction_stats(message.from_user.id)
 
     await message.reply(
-        f'📊 Статистика @{message.from_user.username}\n\n'
-        f'❄️ Снежный денек:\n'
-        f'- брошено снежков: {game_stats[0].throw}\n'
-        f'- получено снежков: {game_stats[0].get}\n\n'
-        f'❄️🔫 Снежная дуэль:\n'
-        f'- выиграно: {game_stats[2].wins} ({game_stats[2].wins_percentage})\n'
-        f'- проиграно: {game_stats[2].losses} ({game_stats[2].losses_percentage})\n'
-        f'- бафф: +{settings.ConfigSnowDuel.user_buff(game_stats[2].amount):.0%}\n\n'
-        f'☃️ Снеговичок:\n'
-        f'- текущий: {game_stats[1].current} см\n'
-        f'- самый высокий: {game_stats[1].maximum} см\n'
-        f'- попыток слепить: {game_stats[1].all_attempts} шт\n\n'
-        f'🔮 Предсказания:\n'
-        f'- получено: {prediction_stats.received}\n\n'
+        localization.get(
+            'stats',
+            tg_username=message.from_user.username,
+            throw=game_stats[0].throw,
+            get=game_stats[0].get,
+            wins=game_stats[2].wins,
+            wins_percentage=game_stats[2].wins_percentage,
+            losses=game_stats[2].losses,
+            losses_percentage=game_stats[2].losses_percentage,
+            buff=f'{settings.ConfigSnowDuel.user_buff(game_stats[2].amount):.0%}',
+            current=game_stats[1].current,
+            maximum=game_stats[1].maximum,
+            all_attempts=game_stats[1].all_attempts,
+            received=prediction_stats.received
+        )
     )
