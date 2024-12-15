9fb48655b07d66e347b9df309137932e38cb84b2
hydrartew <yambarshevartem@yandex.ru>
2024-12-16 02:56:06 +0300

    изменен user_buff.interval_of_games_played на 37

--- configs/settings.py
+++ configs/settings.py
@@ -91,7 +91,7 @@ class Settings(BaseSettings):
                 # opponent=(1-owner)
             ),
             user_buff=UserBuff(
-                interval_of_games_played=45,
+                interval_of_games_played=37,
                 increase_hit_chance="1%",
                 max_increase_hit_chance="15%"
             )
